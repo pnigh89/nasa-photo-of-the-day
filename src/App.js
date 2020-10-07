ea38b4dcc47c91733f5e2372f571acf36b412de7
Peter Nigh <peternigh@Peters-MacBook-Pro.local>
2020-10-07 18:08:47 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,9 +1,28 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./App.css";
+import axios from 'axios'
+import { API_KEY, BASE_URL } from '../src/constant/index'
+// import Header from '../src/components/Header'
+// import PhotoCard from '../src/components/PhotoCard'
 
 function App() {
+
+  const [photoData, setPhotoData] = useState()
+
+  useEffect(()=>{
+    axios.get(`${BASE_URL}?api_key=${API_KEY}`)
+      .then(res =>{
+        setPhotoData(res.data)
+      })
+      .catch(error =>{
+      })
+  }, [])
+
+
   return (
     <div className="App">
+      {/* <Header />
+      <PhotoCard photoData={photoData} /> */}
       <p>
         Read through the instructions in the README.md file to build your NASA
         app! Have fun <span role="img" aria-label='go!'>🚀</span>!
---
+++ src/components/PhotoCard/PhotoCard.js
@@ -0,0 +1,5 @@
+import React from "react"
+import Explanation from '../PhotoCard/Explanation'
+import Photo from '../PhotoCard/Photo'
+import PhotoButtons from '../PhotoCard/PhotoButtons'
+import PhotoHeader from '../PhotoCard/PhotoHeader'
