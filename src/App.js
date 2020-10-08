f4239f83c3b40264a92ce5c44ef3ac9823ff0e43
Peter Nigh <peternigh@Peters-MacBook-Pro.local>
2020-10-08 09:28:51 -0500

    photo is random date now

--- src/App.js
+++ src/App.js
@@ -8,10 +8,11 @@ import PhotoCard from '../src/components/PhotoCard/PhotoCard'
 function App() {
 
   const [photoData, setPhotoData] = useState('')
+  const [date, setDate] = useState('')
 
   useEffect(()=>{
     const fetchPhoto = () => 
-    {axios.get(`${BASE_URL}?api_key=${API_KEY}`)
+    {axios.get(`${BASE_URL}?api_key=${API_KEY}&date=${changeDate}`)
       .then(res =>{
         setPhotoData(res.data)
         
@@ -21,6 +22,15 @@ function App() {
       }
       fetchPhoto()
   }, [])
+
+
+function randomDate(start, end) {
+    return new Date(start.getTime() + Math.random() * (end.getTime() - start.getTime())).toISOString().slice(0,10);
+  
+}
+ let changeDate = randomDate(new Date(1995, 6, 15), new Date())
+ console.log(changeDate)
+
 
 // function App() {
 
@@ -42,10 +52,13 @@ function App() {
 //   }, [])
 
 
+
+
   return (
     <div className="App">
       <Header />
       <PhotoCard photoData={photoData} />
+      <span>Photo of the day for {changeDate} </span> 
       {/* <img src ={photoData.url} /> */}
       
     </div>
--- src/constant/index.js
+++ src/constant/index.js
@@ -1,2 +1,2 @@
 export const BASE_URL = 'https://api.nasa.gov/planetary/apod'
-export const API_KEY = 'vik40foncTAfN26qDL8Sts3jLg73Go64BhcBVSP2&date=2013-03-14'
+export const API_KEY = 'vik40foncTAfN26qDL8Sts3jLg73Go64BhcBVSP2'
